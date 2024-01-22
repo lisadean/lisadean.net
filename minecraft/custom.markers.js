55f609290f6a43ee9ba0b94705ac63706868cde1
Lisa Dean <lisa.dean@supply.com>
2024-01-21 19:19:01 -0500

    update markers

--- minecraft/custom.markers.js
+++ minecraft/custom.markers.js
@@ -21,7 +21,7 @@ Marker format:
         text: marker text do display (in quotes),
         textColor: text color in HTML/CSS format (in quotes),
         offsetX: horizontal pixel offset of the text,
-        offsetY: vertical pixel offset of the text,
+        offsetz: vertical pixel offset of the text,
         font: text font in HTML/CSS format (in quotes),
     },
 
@@ -35,67 +35,140 @@ You can use https://mapmarker.io/editor to generate custom pin images.
 Use the imageScale property if the pin image is too large.
 
 */
-let textStuff = {
-    textColor: 'black',
-    image: 'portal.svg',
-    imageAnchor: [0.5, 1],
-    offsetY: 20,
-    font: 'bold 20px -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol',
+let portalPins = {
+  textColor: 'black',
+  image: 'portal.svg',
+  imageAnchor: [0.5, 1],
+  offsetz: 20,
+  font: 'bold 20px -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol',
 };
 
 UnminedCustomMarkers = {
   isEnabled: true,
-
   markers: [
     {
       x: 1295,
       z: 3482,
       text: 'Stronghold',
-      ...textStuff
+      ...portalPins,
     },
-      {
-          x: 61,
-          z: -276,
-          text: 'Lisa',
-          ...textStuff
-      }
-
-    // Example 1: Simple marker
-    // {
-    //     x: -200,
-    //     z: -200,
-    //     image: "custom.pin.png",
-    //     imageAnchor: [0.5, 1],
-    //     imageScale: 0.5,
-    // },
-
-    // Example 2: Marker with text
-    // {
-    //     x: 0,
-    //     z: 0,
-    //     image: "custom.pin.png",
-    //     imageAnchor: [0.5, 1],
-    //     imageScale: 0.5,
-    //     text: "Marker with text",
-    //     textColor: "red",
-    //     offsetX: 0,
-    //     offsetY: 20,
-    //     font: "bold 20px Calibri,sans serif",
-    // },
-
-    // Example 3: Text only
-    // {
-    //     x: 200,
-    //     z: 200,
-    //     text: "Text only",
-    //     textColor: "yellow",
-    //     offsetX: 0,
-    //     offsetY: 0,
-    //     font: "bold 50px Calibri,sans serif",
-    // },
-      
-    // add your markers here
-
-    // do not delete the following two closing brackets
+    {
+      x: 61,
+      z: -276,
+      text: 'Lisa',
+      ...portalPins,
+    },
+    {
+      x: 79,
+      z: -309,
+      text: 'Mob Farm',
+      ...portalPins,
+    },
+    {
+      x: -466,
+      z: -128,
+      text: 'TradesMart',
+      ...portalPins,
+    },
+    {
+      x: -1494,
+      z: -155,
+      text: 'Paul Home 1',
+      ...portalPins,
+    },
+    {
+      x: -4123,
+      z: -4970,
+      text: 'Snow Village',
+      ...portalPins,
+    },
+    {
+      x: -2593,
+      z: -2455,
+      text: 'Desert Village',
+      ...portalPins,
+    },
+    {
+      x: 234,
+      z: -290,
+      text: 'Summer',
+      ...portalPins,
+    },
+    {
+      x: 203,
+      z: -156,
+      text: 'Ty',
+      ...portalPins,
+    },
+    {
+      x: 368,
+      z: 1412,
+      text: 'Iron Farm',
+      ...portalPins,
+    },
+    {
+      x: 144,
+      z: 2945,
+      text: 'Paul Village 1',
+      ...portalPins,
+    },
+    {
+      x: 5659,
+      z: -1269,
+      text: 'Sarah',
+      ...portalPins,
+    },
+    {
+      x: 5355,
+      z: -5908,
+      text: 'Nether Fortress',
+      ...portalPins,
+    },
+    {
+      x: 579,
+      z: -857,
+      text: 'Djay',
+      ...portalPins,
+    },
+    {
+      x: -4103,
+      z: -923,
+      text: 'Djay Village',
+      ...portalPins,
+    },
   ],
 };
+
+// Example 1: Simple marker
+// {
+//     x: -200,
+//     z: -200,
+//     image: "custom.pin.png",
+//     imageAnchor: [0.5, 1],
+//     imageScale: 0.5,
+// },
+
+// Example 2: Marker with text
+// {
+//     x: 0,
+//     z: 0,
+//     image: "custom.pin.png",
+//     imageAnchor: [0.5, 1],
+//     imageScale: 0.5,
+//     text: "Marker with text",
+//     textColor: "red",
+//     offsetX: 0,
+//     offsetz: 20,
+//     font: "bold 20px Calibri,sans serif",
+// },
+
+// Example 3: Text only
+// {
+//     x: 200,
+//     z: 200,
+//     text: "Text only",
+//     textColor: "yellow",
+//     offsetX: 0,
+//     offsetz: 0,
+//     font: "bold 50px Calibri,sans serif",
+// },
